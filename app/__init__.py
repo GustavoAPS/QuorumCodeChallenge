261c8ea7af3944ba39f959e3654bd4246bfa66ba
Gustavo <gugustavo137@gmail.com>
2024-10-13 14:44:11 -0300

    Fix missing blueprint in routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from app.routes.legislator_routes import legislator_bp
+from app.routes.bill_routes import bill_bp
+
 
 # db = SQLAlchemy()
 
@@ -8,5 +11,8 @@ def create_app():
     app.config.from_object('config.Config')
 
     # db.init_app(app)
+
+    app.register_blueprint(legislator_bp)
+    app.register_blueprint(bill_bp)
 
     return app
